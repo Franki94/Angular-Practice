62e06e009aabb3cf3ce860934a28102bd3fda083
becker oliver utia lopez <becker_utia_123@hotmail.com>
2021-02-27 19:12:51 -0500

    using interface and conditionals in angular

--- platzi-store/src/app/app.component.ts
+++ platzi-store/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  showValue = true;
+
+  items = ['nicolas', 'becker', 'juan'];
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoddie',
+      price: 80000,
+      description: 'bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla'
+    }
+  ];
+
+  addItem(newItem: string): void{
+    this.items.push(newItem);
+  }
+
+  deleteItem(index: number): void{
+    this.items.splice(index, 1);
+  }
 }
